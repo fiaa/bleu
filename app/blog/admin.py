717c60152a5e612d07d8bffc1f204208142234f2
aqua <aqua@webb.fiaa.net>
2023-10-21 23:43:51 +0900

    Allow setting tag and category when creating a post

--- app/blog/admin.py
+++ app/blog/admin.py
@@ -2,11 +2,21 @@ from django.contrib import admin
 
 from .models import Author, Category, Comment, Post, PostCategory, PostTag, Tag
 
+class PostCategoryInline(admin.TabularInline):
+    model = PostCategory
+
+class PostTagInline(admin.TabularInline):
+    model = PostTag
+
+class PostAdmin(admin.ModelAdmin):
+    # filter_horizontal = ("tag", "category")
+    inlines = [PostCategoryInline, PostTagInline]
+
+
 admin.site.register(Author)
 admin.site.register(Category)
 admin.site.register(Comment)
-admin.site.register(Post)
 admin.site.register(Tag)
-
+admin.site.register(Post, PostAdmin)
 admin.site.register(PostCategory)
 admin.site.register(PostTag)
